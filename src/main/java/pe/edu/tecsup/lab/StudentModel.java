059af60b417edaf6541b7830aa9a2eab5dc5d9d6
Janely Apaza Sencia <janelyapazasencia95@gmail.com>
2026-03-28 23:22:08 -0500

    feat(sprint-1): implementación de StudentModel

--- src/main/java/pe/edu/tecsup/lab/StudentModel.java
+++ src/main/java/pe/edu/tecsup/lab/StudentModel.java
@@ -1,5 +1,33 @@
+package pe.edu.tecsup.lab;
+
 public class StudentModel {
+
     private int id;
     private String nombre;
     private String apellido;
+
+    public StudentModel(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("===== DATOS DEL ESTUDIANTE =====");
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
