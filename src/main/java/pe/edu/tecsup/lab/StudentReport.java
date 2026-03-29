dd6abcb45baddf126ffb152dcbdf8a1a38d5cd1b
Washington Berrio <luis.berrio@tecsup.edu.pe>
2026-03-28 23:27:28 -0500

    feat(sprint-1): implementacion de StudentReport

--- src/main/java/pe/edu/tecsup/lab/StudentReport.java
+++ src/main/java/pe/edu/tecsup/lab/StudentReport.java
@@ -1,1 +1,1 @@
-public class StudentReport { public void generateReport(){ System.out.println("Generando reporte..."); } } 
+public class StudentReport { public void generateReport() { System.out.println("--- Reporte del Sprint 1 ---"); System.out.println("Estado: Datos procesados por Colaborador 2."); } } 
