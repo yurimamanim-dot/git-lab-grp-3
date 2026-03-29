ec38eb08d49364978098d53a4e93071914ed901e
Washington Berrio <luis.berrio@tecsup.edu.pe>
2026-03-28 23:45:04 -0500

    feat(sprint-2): mejora en StudentReport

--- src/main/java/pe/edu/tecsup/lab/StudentReport.java
+++ src/main/java/pe/edu/tecsup/lab/StudentReport.java
@@ -1,1 +1,1 @@
-public class StudentReport { public void generateReport(){ System.out.println("Generando reporte..."); } } 
+public class StudentReport { public void generateReport() { System.out.println("--- Reporte del Sprint 2 ---"); System.out.println("Estado: Datos optimizados y procesados."); } } 
